c3852495375790c126e6d070b21e1de66e13eb2b
rob49157 <robertopaipo@hotmail.com>
2022-09-17 12:23:45 -0400

    shopping cart with cheq

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ http://www.coraz0n.com/
 
 # shopping cart 
 https://www.youtube.com/watch?v=lATafp15HWA&ab_channel=WebDevSimplified
+
+https://commercejs.com/docs/community/creating-a-cart-with-react-js/
+
+## ecommerce
+https://dashboard.chec.io/
--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-import { ShoppingCartProvider} from './context/ShoppingCartContext'
+
 
 
 function App() {
   return (
     
-    <ShoppingCartProvider>
+  
       <Router>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          {/* <Route path='/sign-up' component={SignUp} /> */}
         </Switch>
       </Router>
-      </ShoppingCartProvider>
+      
     
   );
 }
