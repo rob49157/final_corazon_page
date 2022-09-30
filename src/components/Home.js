22d86a21d1fdea822183dda235ee2be10b29525d
Benjamin Buzeta <borquezb42@gmail.com>
2022-09-30 19:15:45 -0400

    paypal fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css';
+import Cards from './Cards';
 
 
 function Home() {
   return (
     <>
+
       <img  className="banner" src="images/banner.jpg"/>
       <Cards />
     </>
